7b2a87d1421be422373f50ad6dcea60caaa1b722
Max <98271428+ML02S@users.noreply.github.com>
2024-12-18 16:34:42 +0100

    Add static export for GitHub Pages

---
+++ Portfolio-Max/out/_next/static/chunks/app/not-found-afdb90c8ee883262.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{7501:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,4109,23))},4109:function(){}},function(n){n.O(0,[507,972,971,117,744],function(){return n(n.s=7501)}),_N_E=n.O()}]);
