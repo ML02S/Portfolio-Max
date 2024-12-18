d9bc7ee814c94b7ded5f62e623f1d857ddf11ef2
Max <98271428+ML02S@users.noreply.github.com>
2024-12-18 16:09:02 +0100

    Config aangepast

--- Portfolio-Max/next.config.mjs
+++ Portfolio-Max/next.config.mjs
@@ -1,13 +1,11 @@
-import createMDX from '@next/mdx'
+import createMDX from '@next/mdx';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
+});
 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
