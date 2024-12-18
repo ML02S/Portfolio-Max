7601ee5e25d6e14802d8247a4b143c04c0a0c7b5
Max <98271428+ML02S@users.noreply.github.com>
2024-12-18 16:05:46 +0100

    Pagina toegevoegd

--- Portfolio-Max/src/app/components/Header.tsx
+++ Portfolio-Max/src/app/components/Header.tsx
@@ -1,5 +1,3 @@
-"use client" 
-
 import React, { useState, useEffect, useContext, memo, useRef, useMemo } from "react";
 import { RightArrow } from "./arrows";
 import { CardsContext } from "../cardsContext";
