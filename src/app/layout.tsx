66815dd501f0bfe1923294e6539de12ac9e25d97
Max <98271428+ML02S@users.noreply.github.com>
2025-04-27 16:57:14 +0200

    Update favicon to Fish.png and configure static export settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,29 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "../app/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Portfolio Max",
   description: "Portfolio site of Max Stokla",
   icons: {
-    icon: '/images/Fish.png',
-    apple: '/images/Fish.png',
-  },
+    icon: '/Fish.png',
+    apple: '/Fish.png',
+  }
 };
 
-const inter = Inter({
+const roboto = Roboto_Mono({
   weight: "400",
   subsets: ["latin"],
 });
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+    <html lang="en" className={roboto.className}>
+      <body>{children}</body>
     </html>
   );
 }
